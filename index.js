332dde156010728200e517747f17ffab74c17182
Tim Davis <contact@timdav.is>
2020-11-30 09:10:25 -0800

    Remove extra new line.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = net.createServer(client => {
   console.log(`client #${client.id} connected.`)
   client.setEncoding('utf8')
   client.on('data', data => {
-    console.log(`client #${client.id}: "${data.trim()}"\n`)
+    console.log(`client #${client.id}: "${data.trim()}"`)
     clients.forEach(other => {
       if (client === other) return
       other.write(`client #${client.id}: "${data.trim()}"\n`)
